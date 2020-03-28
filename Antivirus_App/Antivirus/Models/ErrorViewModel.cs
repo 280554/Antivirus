62b9ca48636e84f84fc9833e35257877182d169f
Forsaken <34242359+280554@users.noreply.github.com>
2020-03-28 21:29:45 +0100

    Push de l'extrème

---
+++ Antivirus_App/Antivirus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Antivirus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
